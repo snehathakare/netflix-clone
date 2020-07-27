64000fe9a55ade75d62b268f829660bab27f4c6b
Sneha Lodha <snhthakare@gmail.com>
2020-07-27 18:15:46 +0530

    Adds nav bar scroll functionality

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import './Nav.css';
 
+
 function Nav() {
+  
+  const [show, handleShow] = useState(false);
+
   //to get black background for nav only during scroll
   useEffect(() => {
+    window.addEventListener( "scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
     
+    });
+      return () => {window.removeEventListener("scroll")};  
 
   }, []);
 
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img className="nav__logo" 
        src="https://upload.wikimedia.org/wikipedia/commons/archive/0/08/20190203185210%21Netflix_2015_logo.svg"
        alt="Netflix logo" />
